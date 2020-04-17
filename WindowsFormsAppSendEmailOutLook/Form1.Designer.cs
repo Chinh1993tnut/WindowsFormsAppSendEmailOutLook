66e2c057615bdc74433a8bbe6cc9508563dcc271
Chinh1993tnut <chinh1993tnuttn@outlook.com>
2020-04-17 08:41:17 +0700

    Add project files.

---
+++ WindowsFormsAppSendEmailOutLook/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WindowsFormsAppSendEmailOutLook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBoxMessange = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.checkBoxSendAll = new System.Windows.Forms.CheckBox();
+            this.listBoxTo = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBoxCc = new System.Windows.Forms.ListBox();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.textBoxCc = new System.Windows.Forms.TextBox();
+            this.textBoxFindFile = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(620, 553);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(168, 74);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxMessange
+            // 
+            this.textBoxMessange.Location = new System.Drawing.Point(213, 265);
+            this.textBoxMessange.Multiline = true;
+            this.textBoxMessange.Name = "textBoxMessange";
+            this.textBoxMessange.Size = new System.Drawing.Size(387, 200);
+            this.textBoxMessange.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Message:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cc:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(107, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Subject:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(213, 218);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(387, 26);
+            this.textBoxSubject.TabIndex = 9;
+            this.textBoxSubject.TextChanged += new System.EventHandler(this.textBoxSubject_TextChanged);
+            // 
+            // checkBoxSendAll
+            // 
+            this.checkBoxSendAll.AutoSize = true;
+            this.checkBoxSendAll.Location = new System.Drawing.Point(728, 53);
+            this.checkBoxSendAll.Name = "checkBoxSendAll";
+            this.checkBoxSendAll.Size = new System.Drawing.Size(72, 24);
+            this.checkBoxSendAll.TabIndex = 12;
+            this.checkBoxSendAll.Text = "To all";
+            this.checkBoxSendAll.UseVisualStyleBackColor = true;
+            this.checkBoxSendAll.CheckedChanged += new System.EventHandler(this.checkBoxSendAll_CheckedChanged);
+            // 
+            // listBoxTo
+            // 
+            this.listBoxTo.FormattingEnabled = true;
+            this.listBoxTo.ItemHeight = 20;
+            this.listBoxTo.Items.AddRange(new object[] {
+            "thanhhungbkhn@gmail.com",
+            "chinh18111993@gmail.com",
+            "tieututranduc@gmail.com"});
+            this.listBoxTo.Location = new System.Drawing.Point(213, 33);
+            this.listBoxTo.Name = "listBoxTo";
+            this.listBoxTo.Size = new System.Drawing.Size(387, 44);
+            this.listBoxTo.TabIndex = 13;
+            this.listBoxTo.SelectedIndexChanged += new System.EventHandler(this.listBoxTo_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(728, 135);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 24);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Cc all";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listBoxCc
+            // 
+            this.listBoxCc.FormattingEnabled = true;
+            this.listBoxCc.ItemHeight = 20;
+            this.listBoxCc.Items.AddRange(new object[] {
+            "thanhhungbkhn@gmail.com",
+            "chinh19111993@gmail.com",
+            "tieututranduc@gmail.com"});
+            this.listBoxCc.Location = new System.Drawing.Point(213, 115);
+            this.listBoxCc.Name = "listBoxCc";
+            this.listBoxCc.Size = new System.Drawing.Size(387, 44);
+            this.listBoxCc.TabIndex = 15;
+            this.listBoxCc.SelectedIndexChanged += new System.EventHandler(this.listBoxCc_SelectedIndexChanged);
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(213, 83);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(387, 26);
+            this.textBoxTo.TabIndex = 16;
+            // 
+            // textBoxCc
+            // 
+            this.textBoxCc.Location = new System.Drawing.Point(213, 165);
+            this.textBoxCc.Multiline = true;
+            this.textBoxCc.Name = "textBoxCc";
+            this.textBoxCc.Size = new System.Drawing.Size(387, 47);
+            this.textBoxCc.TabIndex = 17;
+            // 
+            // textBoxFindFile
+            // 
+            this.textBoxFindFile.Location = new System.Drawing.Point(213, 497);
+            this.textBoxFindFile.Name = "textBoxFindFile";
+            this.textBoxFindFile.Size = new System.Drawing.Size(387, 26);
+            this.textBoxFindFile.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(620, 485);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 38);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Dinh kem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 639);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxFindFile);
+            this.Controls.Add(this.textBoxCc);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.listBoxCc);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBoxTo);
+            this.Controls.Add(this.checkBoxSendAll);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMessange);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBoxMessange;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.CheckBox checkBoxSendAll;
+        private System.Windows.Forms.ListBox listBoxTo;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBoxCc;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.TextBox textBoxCc;
+        private System.Windows.Forms.TextBox textBoxFindFile;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsAppSendEmailOutLook/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Outlook;
+using outlook = Microsoft.Office.Interop.Outlook;
+namespace WindowsFormsAppSendEmailOutLook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string ListEmailTo = "";
+        public string ListEmailCc = "";
+        public string ListEmailSubject = "";
+        int nToAll = 0;
+        int nCcAll = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            outlook.Application app = new outlook.Application();
+            outlook.MailItem mail = (outlook.MailItem)app.CreateItem(outlook.OlItemType.olMailItem);
+            //ListEmailTo = listViewTo.TabIndex.ToString();
+            //mail.To = "chinh18111993@gmail.com";
+            //mail.Subject = "Gui thu tu dong";
+            // mail.Body = "Khong co j ";
+            mail.To = textBoxTo.Text;
+            Thread.Sleep(100);
+            mail.CC = textBoxCc.Text;
+            Thread.Sleep(100);
+            mail.Subject = textBoxSubject.Text;
+            Thread.Sleep(100);
+            mail.Body = textBoxMessange.Text;
+            Thread.Sleep(100);
+            if(textBoxFindFile.Text == "")
+            {
+                MessageBox.Show("Khong dinh kem file nao");
+            }
+            else
+            {
+                mail.Attachments.Add(textBoxFindFile.Text);
+            }
+            Thread.Sleep(100);
+            mail.Importance = outlook.OlImportance.olImportanceHigh;
+            Thread.Sleep(100);
+            ((outlook.MailItem)mail).Send();
+            Thread.Sleep(100);
+            MessageBox.Show("Đã gởi Email đến " + textBoxTo.Text + " thành công!");
+
+        }
+        private void checkBoxSendAll_CheckedChanged(object sender, EventArgs e)
+        {
+            nToAll = 1 - nToAll;
+        }
+
+        private void listBoxTo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string curItem = "";
+            curItem = listBoxTo.SelectedItem.ToString();
+            ListEmailTo = curItem;
+            textBoxTo.Text += ListEmailTo.ToString();
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            nCcAll = 1 - nCcAll;
+        }
+
+        private void listBoxCc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string curItem = "";
+            curItem = listBoxCc.SelectedItem.ToString();
+            ListEmailCc = curItem;
+            textBoxCc.Text += ListEmailCc.ToString() + ";";
+        }
+
+        private void textBoxSubject_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileName = ofd.FileName;
+                try
+                {
+                    this.textBoxFindFile.Text = System.IO.Path.GetFullPath(fileName);
+                }
+                catch (System.Exception)
+                {
+                }
+            }
+        }
+    }
+}
